1ad9f5ec1eef6c11f3357bb7043f63bd22314a46
Ndelforno <nicolas.delforno@gmail.com>
2018-10-30 16:10:38 -0400

    step 3 done

--- ajax.js
+++ ajax.js
@@ -2,4 +2,30 @@ document.addEventListener("DOMContentLoaded", function() {
 
   /* Your code goes here */
 
+  var buttonRun = document.getElementById('buttonRun')
+
+  buttonRun.addEventListener('click', function(e){
+    var request = axios({
+      url:'https://intro-ajax-api.herokuapp.com',
+      method: 'get',
+      data: '',
+      dataType: 'text'
+    })
+  });
+
+  var  buttonRunPingPong = document.getElementById('buttonRunPingPong')
+  buttonRunPingPong.addEventListener('click', function(e){
+    var request = axios({
+      url:'https://intro-ajax-api.herokuapp.com/ping',
+      method: 'get',
+      data: '',
+      dataType: 'text'
+    }).then(function(response){
+      console.log(`${response.data}`);
+      var section3456 = document.getElementById('step3456')
+      section3456.append(`${response.data}`)
+    })
+  });
+
+
 });
